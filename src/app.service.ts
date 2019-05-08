29b6fb9e451c05af0b07a01760597f2cd5f744ee
Андрей Овчаренко <ovcharenkoav@meta.ua>
2019-05-08 16:16:01 +0300

    next

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h0>Hello World!</h0>';
+    return 'Hello World!';
   }
 }
--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body, Res, Next } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 interface Params {
   field: number;
+  a: Len;
+}
+
+interface Len {
+    length: number;
 }
 
 @Controller('cats')
 export class CatsController {
-  @HttpCode(201)
-  @Get('all')
-  findAll(@Query() params: Params): string {
-    console.log(params);
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(@Query() params: Params, @Res() res, @Next() next: Function): string {
+    console.log(params.a.length);
+    res.status(201).send('All Ok');
     return 'This action returns all cats';
   }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
 }
